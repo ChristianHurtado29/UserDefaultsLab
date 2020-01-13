32ec864e2d2a706b1333b60cbc6917bd024af179
ChristianHurtado29 <christianhurtado@pursuit.org>
2020-01-13 15:45:05 -0500

    Initial Commit

---
+++ UserDefaultsLab/InitialVC.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  UserDefaultsLab
+//
+//  Created by Christian Hurtado on 1/13/20.
+//  Copyright © 2020 Christian Hurtado. All rights reserved.
+//
+
+import UIKit
+
+class InitialVC: UIViewController {
+
+    @IBOutlet weak var enterNameLabel: UILabel!
+    
+    @IBOutlet weak var nameEntered: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func horroscope(_ sender: UIButton) {
+    }
+    
+
+
+}
+
